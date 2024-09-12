7aff9bffab950a0c3a14cba160a1616a5dde4ff9
Artem Bondar <githublab13@gmail.com>
2024-09-12 13:44:15 -0700

    Create renderStates function

--- script/search.js
+++ script/search.js
@@ -1,1 +1,25 @@
 import { fetchStates } from './fetchStates.js';
+
+async function renderStates() {
+  try {
+    const dataStates = await fetchStates();
+    const ulStates = document.querySelector('#states');
+
+    let searchCatalog = '';
+    Object.keys(dataStates).forEach(state => {
+      searchCatalog += `
+        <li class="the-state-in-search hide searchState${state.trim().replaceAll(' ', '')}">
+          ${state}
+        </li>
+      `;
+    });
+    
+    ulStates.innerHTML = searchCatalog;
+
+    document.querySelector('#state-search-input').addEventListener('input', search);
+    setClickToState();
+
+  } catch (error) {
+    console.error('Error rendering states:', error);
+  }
+}
