d7715bd7c004ba63a5470bf7d3fc835990b53a05
Artem Bondar <githublab13@gmail.com>
2024-09-13 05:03:54 -0700

    Add openSearchMenu function to handle opening the search menu

---
+++ script/searchMenu.js
@@ -0,0 +1,5 @@
+function openSearchMenu() {
+  document.querySelector('.search-menu-open-btn').addEventListener('click', () => {
+    document.querySelector('.search-menu').style.display = "block";
+  });
+};
