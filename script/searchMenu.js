46ccf092010e9009f0b1db630d5c0f4633076f13
Artem Bondar <githublab13@gmail.com>
2024-09-13 05:04:44 -0700

    Add closeSearchMenu function to handle closing the search menu

--- script/searchMenu.js
+++ script/searchMenu.js
@@ -3,3 +3,20 @@ function openSearchMenu() {
     document.querySelector('.search-menu').style.display = "block";
   });
 };
+
+function closeSearchMenu() {
+  document.querySelector('.search-menu-close').addEventListener('click', () => {
+    document.querySelector('#stateSearchInput').value = '';
+    document.querySelector('.search-menu').style.display = "none";
+    clearSearchResult();
+  });
+ 
+  window.addEventListener('click', (event) => {
+    if (event.target.classList.contains('search-menu')) {
+      document.querySelector('#stateSearchInput').value = '';
+     
+      document.querySelector('.search-menu').style.display = "none";
+      clearSearchResult();
+    };
+  });
+};
