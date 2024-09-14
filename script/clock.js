c4e714f89a37cc628cc96242680ae5d3c5187e78
Artem Bondar <githublab13@gmail.com>
2024-09-14 09:13:21 -0700

    Create constructor for CreateClockForState class

--- script/clock.js
+++ script/clock.js
@@ -1,2 +1,10 @@
 import { fetchStates } from './fetchStates.js';
 import { checkWinterOrSummerTime } from './updateSummerAndWinterTime.js';
+
+class CreateClockForState {
+    constructor(name) {
+      this.name = name;
+      this.element = null;
+      this.noSpacesName = this.name.trim().replaceAll(' ', '');
+    };
+};
