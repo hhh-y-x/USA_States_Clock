ee49fcc7c26b44c6e71977360a906adf66d27486
Artem Bondar <githublab13@gmail.com>
2024-09-19 07:53:19 -0700

    create addStatesToDOM function to render states from localStorage

--- script/addStatesToDOM.js
+++ script/addStatesToDOM.js
@@ -1,3 +1,14 @@
 import { SetClockForState } from './clock.js';
 import { checkIsStateInLocalStorage } from './util/localStorage.js';
 
+function addStatesToDOM() {
+  for (let i = 0; i < localStorage.length; i++) {
+    let keyInLocalStorage = localStorage.key(i);
+    if (checkIsStateInLocalStorage(keyInLocalStorage)) {
+      let createAndSetTime = new SetClockForState(localStorage.getItem(keyInLocalStorage));
+       
+      createAndSetTime.render();
+      createAndSetTime.setTimeForState();
+    };
+  };
+};
