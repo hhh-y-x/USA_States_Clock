b2e11a9b6b3ba556da7d3dbdd0fcab2b9a0898fd
Artem Bondar <githublab13@gmail.com>
2024-09-22 11:31:46 -0700

    Create isInAlphabeticalOrder function to fetch and sort states

--- script/alphabeticalOrder.js
+++ script/alphabeticalOrder.js
@@ -1,1 +1,8 @@
 import { fetchStates } from './fetchStates.js';
+
+async function isInAlphabeticalOrder() {
+  const statesData = await fetchStates();
+  const statesArray = Object.keys(statesData).sort();
+
+};
+
