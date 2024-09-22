1955dcfa902be6518d5133e32824bbc890a63e91
Artem Bondar <githublab13@gmail.com>
2024-09-22 11:32:45 -0700

    Get clock for states element

--- script/alphabeticalOrder.js
+++ script/alphabeticalOrder.js
@@ -3,6 +3,9 @@ import { fetchStates } from './fetchStates.js';
 async function isInAlphabeticalOrder() {
   const statesData = await fetchStates();
   const statesArray = Object.keys(statesData).sort();
-
+  
+  const clockForState = document.querySelector('#clock-for-states');
+  
+  
 };
 
