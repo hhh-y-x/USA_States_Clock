b417e4f6824ffe0c55ff01707e87db24736e1836
Artem Bondar <githublab13@gmail.com>
2024-12-24 13:02:36 -0500

    Import and initialize showTooltipBtnForMobile in mainScript.js

--- mainScript.js
+++ mainScript.js
@@ -2,11 +2,14 @@
 
 import { renderStates } from './script/search.js';
 import { isInAlphabeticalOrder } from './script/alphabeticalOrder.js';
+import { showTooltipBtnForMobile } from './script/tooltipForMobile.js';
 import { openSearchMenu, closeSearchMenu } from './script/searchMenu.js';
 
 renderStates();
 
 isInAlphabeticalOrder();
+
+showTooltipBtnForMobile();
 
 openSearchMenu();
 closeSearchMenu();
