150c235b7c31f656fec192a762e4f0c2e79f54fd
Artem Bondar <githublab13@gmail.com>
2024-12-24 12:59:54 -0500

    Add showTooltipBubble function to toggle tooltip bubble visibility

--- script/tooltipForMobile.js
+++ script/tooltipForMobile.js
@@ -8,4 +8,18 @@ function showTooltipBtnForMobile() {
     const bubbleTooltipDiv = document.querySelector('.tooltip-bubble');
 
     tooltipDiv.style.display = 'inline-block';
+
+    function showTooltipBubble(event) {
+        if (event.target.classList.contains('tooltip') || Array.from(descendants).includes(event.target)) {
+            const isBubbleDisplayBlock = window.getComputedStyle(bubbleTooltipDiv).display === 'block';
+
+            if (isBubbleDisplayBlock) {
+                bubbleTooltipDiv.style.display = 'none';
+            } else {
+                bubbleTooltipDiv.style.display = 'block';
+            };
+        } else {
+            bubbleTooltipDiv.style.display = 'none';
+        };
+    };
 };
