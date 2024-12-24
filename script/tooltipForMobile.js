b833b27058f4040a41b799d87ce86f976da7377b
Artem Bondar <githublab13@gmail.com>
2024-12-24 12:56:57 -0500

    Enhance showTooltipBtnForMobile to select tooltip elements and their descendants

--- script/tooltipForMobile.js
+++ script/tooltipForMobile.js
@@ -2,4 +2,8 @@ import { CreateClockForState } from './clock.js';
 
 function showTooltipBtnForMobile() {
     if ( !CreateClockForState.isMobile() ) return;
+
+    const tooltipDiv = document.querySelector('.tooltip');
+    const descendants = tooltipDiv.querySelectorAll('*');
+    const bubbleTooltipDiv = document.querySelector('.tooltip-bubble');
 };
